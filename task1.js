8f9e44bb31b30e45d8f1980883a69a54ce3fe3b4
souju <soujanyamupparapu@gmail.com>
2024-11-27 23:43:38 +0530

    jstask

---
+++ task1.js
@@ -0,0 +1,38 @@
+var age=17;
+if (age>=18){
+    console.log("eligible to vote");
+}else{
+    console.log("not eligible to vote");
+}
+
+    var age=17;
+     if (age >= 18) {
+        console.log("You are eligible to apply for a driving license.");
+    } else {
+        console.log("You are not eligible to apply for a driving license. You must be at least 18 years old.");
+    }
+var girlage=17;
+if(age >=18){
+  console.log("you are eligible to marriage");
+}else{
+    console.log("you are not eligible to marrige");
+}
+var boyage=19;
+if(age >=21){
+    console.log("you are eligible to marriage");
+}else{
+    console.log("you are not eligible to marrige");
+
+}
+var attendence=68;
+if(attendence>=75){
+    console.log("yor are eligible to write a exam");
+}else{
+    console.log("you are not eligible to write a exam");
+}
+var age=60;
+if(age>=60){
+    console.log("qualifies for a senior citizen");
+}else{
+    console.log("not qualifies for a senior citizen");
+}
